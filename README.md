29fbf86d2170618d3b9f2d3bc91db19eecf73008
Michael Ransley <michael.ransley@justnudge.com>
2013-03-16 21:56:09 +1000

    Added in dojo i18n file.

--- README.md
+++ README.md
@@ -4,3 +4,35 @@ Widget i18n Messages
 This repository contains i18n messages for the JustNudge IBM Connections widgets.
 
 To install the mesages follow the information on IBM Connections Wiki [here](http://www-10.lotus.com/ldd/lcwiki.nsf/xpDocViewer.xsp?lookupName=IBM+Connections+4.0+documentation#action=openDocument&res_title=Adding_custom_strings_for_widgets_and_other_specified_scenarios_ic40&content=pdcontent).
+
+The process for installing the messages is as follows:
+
+1. Placing the message files onto the file system.
+2. Defining the messages in the LotusConnections-config.xml file.
+3. Defining the messages in the profiles-config.xml file.
+
+**NOTE:** A list of sample configuration files are included, showing their before and after appearance.  If you need assistance setting up the environment it is recommended that you download the files and diff them with each other (so that you can see the before and after) and also with your configuration.
+
+Placing message files onto the file system
+--
+The files should be be placed into the following directory (example below in Linux):
+
+    /opt/IBM/Connections/data/shared/customization/strings
+
+**NOTE:** Only the properties files need to be placed onto the file system.  The other files describe the installation process.
+
+Defining the messages in the LotusConnections-config.xml file
+--
+Place the following stanza in the "resources" section of the file:
+
+    <widgetBundle prefix="jnmessages" name="com.justnudge.messages" />
+
+Defining the messages in the profiles-config.xml file.
+--
+Modify the templateNlsBundles to include the jnmessages bundle as shown below:
+
+    <templateNlsBundles>jnmessages</templateNlsBundles>
+
+Javascript i18n files
+==
+This project includes the i18n Dojo JSON files that are using by the JustNudge commercial and open source widgets.  These sources will by default download from github when the widgets are installed or can be copied and used locally.  For further details on this see the appropriate widget documentation.
---
+++ nls/jnmessages.js
@@ -0,0 +1,82 @@
+/*******************************************************************************
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+
+* http://www.apache.org/licenses/LICENSE-2.0
+
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+******************************************************************************/
+{
+
+	// Common Tags
+	jnCommonYes: "Yes",
+	jnCommonNo: "No",
+	jnCommonError: "An error occurred with this widget, please contact your system administrator.",
+
+	// Organization Tags Messages
+	jnorgtagsUpdateSuccess: "The tags have been update successfully.",
+	jnorgtagsNoTags: "You must choose some new tags to add to the profile.",
+	jnorgtagsSave: "Save",
+
+	// Other Manager Messages
+	jnOtherManagersProfileNoReports: "${0} does not have other reports.",
+	jnOtherManagersProfileNoManagers: "${0} does not have other managers.",
+	jnOtherManagersProfileIDoNotReportTo: "I do not report to ${0}.",
+	jnOtherManagersProfileDoesNotReportToMe: "${0} does not report to me.",
+	jnOtherManagersProfileIReportTo: "I report to ${0}",
+	jnOtherManagersProfileReportsToMe: "${0} reports to me.",
+	jnOtherManagersConfirmReportCreate: "Are you sure that ${0} reports to you?",
+	jnOtherManagersConfirmReportRemove: "Are you sure that ${0} no longer reports to you?",
+	jnOtherManagersConfirmManagerCreate: "Are you sure that ${0} is your manager?",
+	jnOtherManagersConfirmManagerRemove: "Are you sure that ${0} is no longer your manager?",
+	jnOtherManagersManagersHeader: "${0} is also managed by:",
+	jnOtherManagersReportsHeader: "${0} also manages:",
+
+	// Profile Progress Messages
+	jnProfileProgressStatistics: "Statistics",
+	jnProfileProgressSuccess: "This profile is 100% complete.",
+	jnProfileProgressFailure: "Unable to calculate profile progress.",
+	jnProfileProgressTags: "This profile could be improved by adding some tags.",
+	jnProfileProgressConnections: "This profile could be improved by adding some people to your network.",
+	jnProfileProgressPhoto: "This profile could be improved by adding a photo.",
+	jnProfileProgressStatus: "This profile could be improved by adding some status updates.",
+	jnProfileProgressAboutMe: "This profile could be improved by adding some information in about me.",
+	jnProfileProgressExperience: "This profile could be improved by adding some experience.",
+
+	// Update Manager Messages
+	jnUpdateManagerTopDownNoManager: "You do not currently manage ${0}.",
+	jnUpdateManagerTopDownManage: "I currently manage ${0}.",
+	jnUpdateManagerTopDownConfirm: "Are you sure that you currently manage ${0}?",
+	jnUpdateManagerTopDownSuccess: "The manager has been updated successfully.  The reporting chain will update shortly.",
+	jnUpdateManagerTopDownOwnProfile: "Managers can update a person's profile.  If you are a manager, locate the profile of the person you manage and select the link.",
+
+	jnUpdateManagerBottomUpNotManager: "You do not currently report to ${0}.",
+	jnUpdateManagerBottomUpIsManager: "I currently report to ${0}.",
+	jnUpdateManagerBottomUpOwnProfile: "To update your manager, locate their profile and select the link.",
+	jnUpdateManagerBottomUpConfirm: "Are you sure that you currently report to ${0}.",
+	jnUpdateManagerBottomUpSuccess: "The manager has been updated successfully.  The reporting chain will update shortly.",
+
+	jnUpdateManagerIDoNotManage_profile: "You do not currently manage ${0}.",
+	jnUpdateManagerIDoManageProfile: "You currently manage ${0}.",
+	jnUpdateManagerIManageProfile: "I manage ${0}.",
+	jnUpdateManagerConfirm_manage: "Are you sure that you currently manage ${0}?",
+	jnUpdateManagerSuccessfulUpdate: "The manager has been updated successfully.  The reporting chain will be updated shortly.",
+	jnUpdateManagerOwnProfile: "Managers can update a person's profile. If you are a manager select the person's profile and click on the link.",
+	jnUpdateManagerMessageConfirmYes: "Yes",
+	jnUpdateManagerMessageConfirmNo: "No",
+
+	// Twitter Messages
+	jnTwitterLoading: "Loading tweets...",
+	jnTwitterNoName: "No twitter name specified"
+
+}
